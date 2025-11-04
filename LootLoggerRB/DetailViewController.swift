141a13a262c092df24563bf02babc500f7ba297e
Professor Breaker <jbreak1@ju.edu>
2025-11-04 15:28:52 -0500

    Presentation Styles

--- LootLoggerRB/DetailViewController.swift
+++ LootLoggerRB/DetailViewController.swift
@@ -17,6 +17,9 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
                                                     message: nil,
                                                     preferredStyle: .actionSheet)
         
+        alertController.modalPresentationStyle = .popover
+        alertController.popoverPresentationController?.barButtonItem = sender
+        
         let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
                 print("Present camera")
             }
